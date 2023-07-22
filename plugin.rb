dc08549b5d1c0e36b8c4936b31dd238336010892
Richard <richard@communiteq.com>
2023-07-22 11:14:58 +0000

    Initial commit

---
+++ plugin.rb
@@ -0,0 +1,5 @@
+# name: discourse-gitcoin-passport
+# about: Communiteq Gitcoin Passport plugin
+# version: 1.0
+# authors: richard@communiteq.com
+# url: https://github.com/communiteq/discourse-gitcoin-passport
